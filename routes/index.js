1fb07703e30c966574497d91f36972e8a41a4be3
Hank Chu <drunkard960027@gmail.com>
2019-12-16 19:00:08 +0800

    add firebase

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../testjs1125-firebase-adminsdk-yczu1-53b501ed91.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://testjs1125.firebaseio.com"
+});
+
+let db = admin.firestore();
+
+// console.log(db);
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
