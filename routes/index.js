4a835e1c26c03bc72bfbfa793d9ffe72f6842839
Hank Chu <drunkard960027@gmail.com>
2020-01-02 01:03:33 +0800

    update data

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,78 @@
 var express = require('express');
 var router = express.Router();
 
-
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../testjs1125-firebase-adminsdk-yczu1-53b501ed91.json");
+var serviceAccount = require("../nukjsfinal-firebase-adminsdk-fn2w3-142e7ebe10.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://testjs1125.firebaseio.com"
+  databaseURL: "https://nukjsfinal.firebaseio.com"
 });
 
 let db = admin.firestore();
 
-// console.log(db);
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var roomRef = db.collection('room');
+	
+	// console.log(serviceRef);
+	var roomData = [];
+	var serviceData=[]; 	
+	  	roomRef.get().then(function (querySnapshot) { 
+	    	querySnapshot.forEach(function (doc) {
+	    		console.log(doc.id);
+	    		let serviceRef = roomRef.doc(doc.id).collection("service");
+	    		serviceRef.get().then(function(querySnapshot){
+	    			querySnapshot.forEach(function(docS){
+	    				
+	    				serviceData.push(docS.data());
+	    				
+	    			});
+	    			// console.log(serviceData);
+	    		});
+
+	      		roomData.push(doc.data());
+	    	});
+	    	
+	    	console.log(roomData);
+	    	res.render('index', { data : roomData });
+  		});
+  		
+});
+
+
+
+router.post('/detail',function(req,res){
+	var roomRef = db.collection('room');
+	
+	// console.log(serviceRef);
+	var roomData = [];
+	var serviceData=[]; 	
+	  	roomRef.where('name','==',req.body.name).get().then(function (querySnapshot) { 
+	    	querySnapshot.forEach(function (doc) {
+	    		console.log(doc.id);
+	    		let serviceRef = roomRef.doc(doc.id).collection("service");
+	    		serviceRef.get().then(function(querySnapshot){
+	    			querySnapshot.forEach(function(docS){
+	    				
+	    				serviceData.push(docS.data());
+	    				
+	    			});
+	    			console.log(serviceData);
+	    			roomData.push(doc.data());
+	    			console.log(roomData);
+	    			res.render('detail', { rData : roomData , sData : serviceData });
+	    		});
+
+	      		
+	    	});
+	    	// console.log(serviceData);
+	    	
+	    	
+  		});
+	console.log(req.body.name);
 });
 
 module.exports = router;
